381777f7ef7cbe505e1a60fde792490229e8f7e5
Fangray <fulola@qq.com>
2018-03-31 13:04:46 +0800

    Initial commit

---
+++ blog/sendemail.py
@@ -0,0 +1,7 @@
+from django.core.mail import send_mail
+from django.conf import settings
+
+settings.configure()
+send_mail('tugodfafu','nisfsfde4sfsfsfhaoma','email@ihua.com',['fulola@qq.com'])
+
+
